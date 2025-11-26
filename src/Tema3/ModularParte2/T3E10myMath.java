00cac41a71c3302153b72e038a166782387a0ec3
inicermar <inicermar@alu.edu.gva.es>
2025-11-26 13:18:12 +0100

    homerunner

--- src/Tema3/ModularParte2/T3E10myMath.java
+++ src/Tema3/ModularParte2/T3E10myMath.java
@@ -2,15 +2,138 @@ package Tema3.ModularParte2;
 
 import java.util.Scanner;
 
+import static Tema3.ModularParte2.myMath.*;
+
 public class T3E10myMath {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String a = sc.next();
-        for(int i=0;i<a.length();i++) {
-            int par = Integer.parseInt(a.charAt(a.length()-1-i) + "");
-            if (par % 2 == 1) {
-                System.out.print(par);
+        char getout='N', eleccion, choose;
+        String str1;
+        int num1, num2, num3, fail;
+        //inicialización de variables
+        while (getout == 'N'||getout=='n') {
+            globalMenuP2();
+            eleccion = sc.next().charAt(0);
+            switch (eleccion) {
+                case 'a'://Ejercicio 1
+                    System.out.println("Elige: a) circulo, b) cuadrado, c) rectangulo");
+                    choose = sc.next().charAt(0);
+                    switch(choose) { //elección de modulos del ejercicio 1 los cuales hacen el trabajo
+                        case 'a':
+                            System.out.println("Introduce Radio: ");
+                            num1 = sc.nextInt();
+                            System.out.println("Area: " + circleArea(num1) + "\nPerimetro: " + circlePerimeter(num1));
+                            break;
+                        case 'b':
+                            System.out.println("Introduce Lado: ");
+                            num1 = sc.nextInt();
+                            System.out.println("Area: " + squareArea(num1) + "\nPerimetro: " + squarePerimeter(num1));
+                            break;
+                        case 'c':
+                            System.out.println("Introduce Base: ");
+                            num1 = sc.nextInt();
+                            System.out.println("Introduce Altura: ");
+                            num2 = sc.nextInt();
+                            System.out.println("Area: " + rectangleArea(num1, num2) + "\nPerimetro: " + rectanglePerimeter(num1, num2));
+                            break;
+                        default:
+                            System.out.println("Opción invalida, Saliendo.");
+                            break;
+                    }
+                    break;
+                case 'b'://Ejercicio 2
+                    System.out.println("Elige: a) Primos, b) No primos");
+                    choose = sc.next().charAt(0);
+                    switch(choose){
+                        case 'a':
+                            System.out.println("Introduce un número y te diré si es primo");
+                            num1 = sc.nextInt();
+                            if (num1 > 0) {
+                                num2 = 2;
+                                fail = 0;
+                                while (num1 != num2) {
+                                    if (!isPrime(num1, num2)) {//modulo encargado de asegurarse de que es primo
+                                        fail++;//variable que sube si no es primo
+                                    }
+                                    num2++;
+                                }
+                                if (fail == 0) {
+                                    System.out.println("El numero " + num1 + " es primo.");
+                                }
+                            } else {
+                                System.out.println("El numero no era positivo.");
+                            }
+                            break;
+                        case 'b':
+                            System.out.println("Introduce un número y te diré si no es primo");
+                            num1 = sc.nextInt();
+                            if (num1 > 0) {
+                                num2 = 2;
+                                fail = 0;
+                                while (num1 != num2) {
+                                    if (!isNotPrime(num1, num2)) {//modulo encargado de asegurarse de que es primo
+                                        fail++;//variable que sube si es primo
+                                    }
+                                    num2++;
+                                }
+                                if (fail == 0) {
+                                    System.out.println("El numero " + num1 + " no es primo.");
+                                }
+                            } else {
+                                System.out.println("El numero no era positivo.");
+                            }
+                            break;
+                        default:
+                            System.out.println("Opción invalida, Saliendo.");
+                            break;
+                    }
+                    break;
+                case 'c'://Ejercicio 3
+                    System.out.println("Introduce un número: ");
+                    num1 = sc.nextInt();
+                    longitudNumero(num1);
+                    break;
+                case 'd'://Ejercicio 4
+                    System.out.println("Introduce un número: ");
+                    str1 = sc.next();
+                    paresEnNumero(str1);
+                    break;
+                case 'e'://Ejercicio 5
+                    System.out.println("Introduce un número: ");
+                    str1 = sc.next();
+                    imparesEnNumero(str1);
+                    break;
+                case 'f'://Ejercicio 6
+                    System.out.println("Introduce un número: ");
+                    num1 = sc.nextInt();
+                    System.out.println("El factorial de "+num1+" es " +factorial(num1));
+                    break;
+                case 'g'://Ejercicio 7
+                    System.out.println("Introduce un número: ");
+                    num1 = sc.nextInt();
+                    num2 = factorialRecursive(num1);
+                    System.out.println("El factorial de "+num1+" es " +num2);
+                    break;
+                case 'h'://Ejercicio 8
+                    System.out.println("Introduce el primer coeficiente (a): ");
+                    num1 = sc.nextInt();
+                    System.out.println("Introduce el segundo coeficiente (b):");
+                    num2 = sc.nextInt();
+                    System.out.println("Introduce el tercer coeficiente (c):");
+                    num3 = sc.nextInt();
+                    segundoGrado(num1, num2, num3);
+                    break;
+                case 'i'://Ejercicio 9
+                    System.out.println("Introduce un número:");
+                    str1 = sc.next();
+                    sumaEntreNumeros(str1);
+                    break;
+                default:
+                    System.out.println("Opción no valida.");
+                    break;
             }
+            System.out.println("¿Quieres salir? (Y/N)"); //linea encargada de salir
+            getout=sc.next().charAt(0);// cualquier otro carácter hace lo mismo que Y, lo cual es salir
         }
     }
 }
--- src/Tema3/ModularParte2/myMath.java
+++ src/Tema3/ModularParte2/myMath.java
@@ -30,24 +30,32 @@ public class myMath {
     }
     /// ----------------------EJERCICIO 3
     public static void longitudNumero(int a) {
-        int b = Integer.toString(a).length();
-        System.out.println("El número " + a + " tiene " + b + " dígitos");
+        int divis = 1;
+        while (a > 10) {
+            a = a/10;
+            divis++;
+        }
+        if (divis <= 1){
+            System.out.println("Tu número tiene "+divis+" cifra.");
+        }else {
+            System.out.println("Tu número tiene "+divis+" cifras.");
+        }
     }
     /// ----------------------EJERCICIO 4
     public static void paresEnNumero(String a) {
         for(int i=0;i<a.length();i++) {
             int par = Integer.parseInt(a.charAt(a.length()-1-i) + "");
             if (par % 2 == 0) {
-                System.out.print(par);
+                System.out.println("Numero par: "+ par);
             }
         }
     }
     /// ----------------------EJERCICIO 5
     public static void imparesEnNumero(String a) {
         for (int i = 0; i < a.length(); i++) {
-            int par = Integer.parseInt(a.charAt(a.length() - 1 - i) + "");
-            if (par % 2 == 1) {
-                System.out.print(par);
+            int impar = Integer.parseInt(a.charAt(a.length() - 1 - i) + "");
+            if (impar % 2 == 1) {
+                System.out.println("Numero impar: " + impar);
             }
         }
     }
@@ -61,10 +69,56 @@ public class myMath {
     }
     /// ----------------------EJERCICIO 7
     public static int factorialRecursive(int a) {
-        int b = a;
-        for (int i = 1; i <= a; b--) {
-            b = b * i;
+        if (a <= 1) {
+            return 1;
+        }else{
+            return a * factorialRecursive(a-1);
         }
-        return b;
+    }
+    /// ----------------------EJERCICIO 8
+    public static void segundoGrado(double a, double b, double c) {
+        double discriminant, raizCuadrada, res1, res2;
+        discriminant = (b*b) - (4 * a * c);
+        if (discriminant > 0) {
+            raizCuadrada = Math.sqrt(discriminant);
+            res1 = (0-b +raizCuadrada) / (2*a);
+            res2 = (0-b -raizCuadrada) / (2*a);
+            System.out.println("El discriminante es: " + discriminant);
+            System.out.println("Hay dos soluciones:");
+            System.out.println("Resultado 1: " + res1);
+            System.out.println("Resultado 2: " + res2);
+        } else if (discriminant == 0) {
+            res1 = -b / (2*a);
+            System.out.println("Resultado 1: " + res1);
+            System.out.println("Resultado 2: nil");
+        } else {
+            System.out.println("Resultado 1: nil");
+            System.out.println("Resultado 2: nil");
+        }
+
+    }
+    /// ----------------------EJERCICIO 9
+    public static void sumaEntreNumeros(String a) {
+        int suma = 0, par;
+        for(int i=0;i<a.length();i++) {
+            par = Integer.parseInt(a.charAt(a.length()-1-i) + "");
+            suma = suma + par;
+        }
+        System.out.println("la suma total es: " + suma);
+    }
+    /// ----------------------MENU
+    public static void globalMenuP2() {
+        System.out.println("Menu global.");//menu global
+        System.out.println("--------------------------------------");
+        System.out.println("a - Calcular el perímetro y el área de un círculo, cuadrado y rectángulo");
+        System.out.println("b - Comprobador de números primos");
+        System.out.println("c - indicador de dígitos de un número entero");
+        System.out.println("d - indicador de dígitos pares de un número entero");
+        System.out.println("e - indicador de dígitos impares de un número entero");
+        System.out.println("f - Calculador del factorial de un número");
+        System.out.println("g - Calculador del factorial recursivo de un número");
+        System.out.println("h - Calculador de ecuaciones de segundo grado");
+        System.out.println("i - Sumatorio de digitos de un número entero");
+        System.out.println("Otra opción - Salir");
     }
 }
