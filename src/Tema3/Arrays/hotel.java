bfc35132c754934ff45e4b56988c9d55dfcd410e
inicermar <inicermar@alu.edu.gva.es>
2025-12-04 12:00:21 +0100

    homerunner

---
+++ src/Tema3/Arrays/hotel.java
@@ -0,0 +1,14 @@
+package Tema3.Arrays;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class hotel {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int[] hotel = new int[20];
+        int wifi, hap;
+        Arrays.fill(hotel, 0);
+
+    }
+}
