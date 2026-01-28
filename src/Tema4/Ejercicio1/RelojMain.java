697cd4c8ae16694628e2bce3444702b24a5d945d
inicermar <inicermar@alu.edu.gva.es>
2026-01-28 11:38:19 +0100

    never forget

--- src/Tema4/Ejercicio1/RelojMain.java
+++ src/Tema4/Ejercicio1/RelojMain.java
@@ -3,11 +3,16 @@ package Tema4.Ejercicio1;
 public class RelojMain {
     public static void main(String[] args){
         Reloj reloj = new Reloj(3,54,45);
+        Reloj reloj1 =  new Reloj();
         reloj.getS();
         reloj.getM();
         reloj.getH();
         System.out.println(reloj);
+        reloj1.getS();
+        reloj1.getM();
+        reloj1.getH();
+        System.out.println(reloj1);
         reloj.toString();
-        System.out.println(reloj);
+        reloj1.toString();
     }
 }
