3ad03b0262717aaaa405fa53978ec0f0dbc43faa
inicermar <inicermar@alu.edu.gva.es>
2026-02-02 10:40:19 +0100

    never forget

--- src/Tema4/POO2/Persona.java
+++ src/Tema4/POO2/Persona.java
@@ -1,17 +1,20 @@
 package Tema4.POO2;
 
-public class Persona {
+public class Persona{
     private String nombre;
     private final String dni;
+    private Cuenta[] cuenta;
 
     public Persona(){
         this.dni = null;
         this.nombre = "pepe";
+        this.cuenta = new Cuenta[3];
     }
 
     public Persona(String nombre, String dni){
         this.nombre = nombre;
         this.dni = dni;
+        this.cuenta = new Cuenta[3];
     }
 
     public String getNombre() {
@@ -22,5 +25,11 @@ public class Persona {
     }
     public String getDni() {
         return dni;
+    }
+    public Cuenta[] getCuenta() {
+        return cuenta;
+    }
+    public void setCuenta(int i, Cuenta[] cuenta) {
+        this.cuenta[i] = cuenta[i];
     }
 }
--- src/Tema4/POO2/PruebaCuentas.java
+++ src/Tema4/POO2/PruebaCuentas.java
@@ -1,7 +1,75 @@
 package Tema4.POO2;
 
-public class PruebaCuentas {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class PruebaCuentas {
+    public static void menu(){
+        //bread
+        System.out.println("Menu principal");
+        System.out.println("-------------------------------------");
+        System.out.println("a. Crear nueva persona");
+        System.out.println("b. Crear Cuenta y asociarla a una persona");
+    }
+    public static void main(String[] args) {
+        Persona[] persona = new Persona[3];
+        Cuenta[] cuenta = new Cuenta[9];
+        String nombre, dni;
+        int pos, numc;
+        boolean loop = true;
+        Scanner sc = new Scanner(System.in);
+        while(loop){
+            menu();
+            char doodad;
+            doodad = sc.next().charAt(0);
+            doodad = Character.toUpperCase(doodad);
+            switch(doodad){
+                case 'A':
+                    System.out.println("Ingrese el nombre de la persona");
+                    nombre = sc.next();
+                    System.out.println("Ingrese el dni de la persona");
+                    dni = sc.next();
+                    System.out.println("Elige posicion de la persona (1-3)");
+                    pos = sc.nextInt() - 1;
+                    if(pos >= 0 && pos <= 2){
+                        persona[pos] = new Persona(nombre,dni);
+                    }else{
+                        System.out.println("Posición invalida, persona no creada.");
+                    }
+                    break;
+                case 'B':
+                    boolean check = false;
+                    System.out.println("¿A quien le va a pertenecer la cuenta (1-3)?");
+                    pos = sc.nextInt() - 1;
+                    if(pos >= 0 && pos <= 2&&persona[pos] != null){
+                        System.out.println("Introduce un número de cuenta");
+                        numc = sc.nextInt();
+                        for(int i = 0; i < cuenta.length; i++){
+                            if(cuenta[i] == null&&!check){
+                                cuenta[i] = new Cuenta(numc);
+                                persona[pos].setCuenta(i,cuenta);
+                                check = true;
+                            }
+                        }
+                    }else{
+                        if(persona[pos] == null){
+                            System.out.println("No hay nada ahi.");
+                        }
+                        System.out.println("Posición invalida, cuenta no creada.");
+                    }
+                    break;
+                case 'C':
+                    break;
+                case 'D':
+                    break;
+                case 'E':
+                    break;
+                case 'F':
+                    break;
+                case 'G':
+                    break;
+                default:
+                    loop = false;
+            }
+        }
     }
 }
