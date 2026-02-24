26639cbe963cdfd8b02864bbff456421009c64ca
inigo <inicermar@alu.edu.gva.es>
2026-02-24 23:50:35 +0100

    Para quitar el "todo"

--- src/tema6/Ej3/Cuadrado.java
+++ src/tema6/Ej3/Cuadrado.java
@@ -3,7 +3,7 @@ package tema6.Ej3;
 public class Cuadrado extends Rectangulo {
 
     public Cuadrado(double lado) {
-        //esto lo hace más facil ya que si hereda rectangulo lo tiene todo
+        //esto lo hace más facil ya que si hereda rectangulo lo tiene to.do
         super(lado, lado);
     }
     //menos el toString pero eso se arregla
