4acfa6fcffdb25d5cc1ffc5b81d234772285d05b
inicermar <inicermar@alu.edu.gva.es>
2026-03-23 10:18:00 +0100

    me estoy mudando y solo he podido hacer esto perdon

---
+++ src/tema7/Documentos/Ejercicios/ejercicioB6.java
@@ -0,0 +1,14 @@
+package tema7.Documentos.Ejercicios;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class ejercicioB6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce un número para ver si aparece en pi");
+        int num = sc.nextInt();
+        File pi = new File("/media/inicermar/flarp/IdeaProjects/ProgramacionInigo/src/tema7/Documentos/pi-million.txt");
+
+    }
+}
