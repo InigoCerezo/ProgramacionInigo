0c678c0a63cdfbc63d08940e8aab1bd0eb6fbb27
inicermar <inicermar@alu.edu.gva.es>
2026-03-23 10:49:36 +0100

    consistencia (?)

--- src/tema7/Documentos/Ejercicios/ejercicioB6.java
+++ src/tema7/Documentos/Ejercicios/ejercicioB6.java
@@ -1,14 +1,44 @@
 package tema7.Documentos.Ejercicios;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class ejercicioB6 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
+        boolean check = false;
+        int pos = 0;
         System.out.println("Introduce un número para ver si aparece en pi");
         int num = sc.nextInt();
-        File pi = new File("/media/inicermar/flarp/IdeaProjects/ProgramacionInigo/src/tema7/Documentos/pi-million.txt");
-
+        String search = String.valueOf(num);
+        String pi = "/media/inicermar/flarp/IdeaProjects/ProgramacionInigo/src/tema7/Documentos/pi-million.txt";
+        Path path = Paths.get(pi);
+        String contenido = Files.readString(path);
+        char[] pees = contenido.toCharArray();
+        char[] tgt = search.toCharArray();
+        for (int i = 0; i <= pees.length - tgt.length; i++) {
+            boolean b = true;
+            for (int j = 0; j < tgt.length; j++) {
+                if (pees[i + j] != tgt[j]) {
+                    b = false;
+                    break;
+                }
+            }
+            if (b) {
+                check = true;
+                pos = i;
+                break;
+            }
+        }
+        if (check) {
+            System.out.println("La secuencia '"+num+"' aparece en la posición '"+pos+"' de pi.");
+        }else{
+            System.out.println("La secuencia '"+num+"' no aparece en pi.");
+        }
+        sc.close();
     }
 }
