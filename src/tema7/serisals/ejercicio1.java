08f89091d2ce9886a491198cee3d973716f05b1c
inicermar <inicermar@alu.edu.gva.es>
2026-03-26 10:52:17 +0100

    turmoil

--- src/tema7/serisals/ejercicio1.java
+++ src/tema7/serisals/ejercicio1.java
@@ -5,25 +5,26 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+import java.util.Scanner;
 
 public class ejercicio1 {
     public static void main(String[] args) {
-        String fileName = "datos.txt";
+        Scanner sc = new Scanner(System.in);
+        String fileName = "usuario.txt";
         //String fileName = "/media/inicermar/flarp/IdeaProjects/ProgramacionInigo/src/tema7/datos.txt";
-        Path filePath = Paths.get("datos.txt");
-
-
-        //Files.writeString
+        Path filePath = Paths.get(fileName);
         try {
-            //Escribe contenido al fichero, borrando todo lo que haya
-            Files.writeString(filePath, "Aquí escribo borrando todo el contendio anterior\n");
-            //Escribe contenido al fichero, añadiendolo al final del mismo
-            Files.writeString(filePath, "Aquí escribo añadiendo al final", StandardOpenOption.APPEND);
-
+            System.out.println("Introduce tu nombre:");
+            String nombre = sc.nextLine();
+            System.out.println("Introduce tu edad:");
+            String edad = sc.nextLine();
+            System.out.println("Escribiendo en usuario.txt");
+            //Files.writeString
+            Files.writeString(filePath, ("Nombre: "+nombre + "\nEdad: "+edad), StandardOpenOption.APPEND,StandardOpenOption.CREATE);
             //Verify file content
             String content = Files.readString(filePath);
-
             System.out.println(content);
+            //está todo copiado del escrituralecturatexto
         } catch (IOException e) {
             e.printStackTrace();
         }
